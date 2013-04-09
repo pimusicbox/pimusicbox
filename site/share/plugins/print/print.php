0c4fb47e0c316b53d521b379f4ae0dda73aa92b4
Wouter van Wijk <woutervanwijk@gmail.com>
2013-04-09 23:01:41 +0200

    beta update

--- site/share/plugins/print/print.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/../../includes/global.inc.php');
-
-header('Content-Type: text/html');
-echo $raw_content . '<script type="text/javascript">window.print();</script>';
-
-?>
