0309e2962011c5afb4c02c9e5c42d42ef5710d3f
woutervanwijk <wouter@iMac-3.local>
2014-10-09 12:41:40 +0200

    upmpdcli as upnp renderer instead of gmediarender

---
+++ filechanges/opt/musicbox/prepimage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# MusicBox image clean and preparation script
+#
+apt-get remove build-essential git gcc cpp debian-reference-common g++ make ttf-bitstream-vera linux-libc-dev python-dev
+apt-get autoremove
+apt-get autoclean
+apt-get clean
