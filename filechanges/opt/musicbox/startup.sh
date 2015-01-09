1e5b09282c7df7b83c12e05083cfa0281bf4486c
jcass <john.cass77@gmail.com>
2015-01-09 11:40:09 +0200

    Make samba share mounting routine Mac OSX-compatible

--- filechanges/opt/musicbox/startup.sh
+++ filechanges/opt/musicbox/startup.sh
@@ -180,12 +180,12 @@ done
 # set date/time
 ntpdate ntp.ubuntu.com > /dev/null 2>&1 || true
 
-#mount windows share
+#mount samba share
 if [ "$INI__network__mount_address" != "" ]
 then
-    #mount samba share, readonly
-    log_progress_msg "Mounting Windows Network drive..." "$NAME"
-    mount -t cifs -o sec=ntlm,ro,user=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
+    #mount readonly
+    log_progress_msg "Mounting Samba share..." "$NAME"
+    mount -t cifs -o sec=ntlmssp,ro,user=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
 #    mount -t cifs -o sec=ntlm,ro,rsize=2048,wsize=4096,cache=strict,user=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
 #add rsize=2048,wsize=4096,cache=strict because of usb (from raspyfi)
 fi
