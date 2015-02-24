7a5c858edc7f5c9041daaa5ff4ec3edb4761efd7
jcass <john.cass77@gmail.com>
2015-02-24 08:14:45 +0200

    Revert to master.

--- filechanges/opt/musicbox/startup.sh
+++ filechanges/opt/musicbox/startup.sh
@@ -180,12 +180,12 @@ done
 # set date/time
 ntpdate ntp.ubuntu.com > /dev/null 2>&1 || true
 
-#mount samba share
+#mount windows share
 if [ "$INI__network__mount_address" != "" ]
 then
-    #mount readonly
-    log_progress_msg "Mounting Network drive..." "$NAME"
-    mount -t cifs -o sec=ntlmssp,ro,rsize=2048,wsize=4096,cache=strict,nounix,noperm,username=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
+    #mount samba share, readonly
+    log_progress_msg "Mounting Windows Network drive..." "$NAME"
+    mount -t cifs -o sec=ntlm,ro,user=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
 #    mount -t cifs -o sec=ntlm,ro,rsize=2048,wsize=4096,cache=strict,user=$INI__network__mount_user,password=$INI__network__mount_password $INI__network__mount_address /music/Network/
 #add rsize=2048,wsize=4096,cache=strict because of usb (from raspyfi)
 fi
