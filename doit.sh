f3accf11eeab0240a6680c4284c9c940a85f341a
Nick Steel <nickasteel@gmail.com>
2017-07-27 23:31:28 +0100

    Remove accidentally committed file

--- doit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-APT_PROXY=`hostname`:3142
-cp pimusicbox-0.6.0/musicbox0.6.img .
-./pimusicbox/makeimage.sh musicbox0.6.img bigger
-./pimusicbox/chroot.sh musicbox0.6.img create_musicbox0.7.sh
-
