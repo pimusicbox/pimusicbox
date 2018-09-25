f2b89ba45e94ce2233a1885eb395adc5bc17773f
mrmajik45 <35823544+mrmajik45@users.noreply.github.com>
2018-09-25 11:19:51 -0400

    Multiple problems fixed.
    Maintainability:
    1. Final newline missing
    2. Subprocess call with shell=True identified, security issue.

--- filechanges/root/updatepip.py
+++ filechanges/root/updatepip.py
@@ -2,4 +2,4 @@ import pip
 from subprocess import call
 
 for dist in pip.get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("pip install --upgrade " + dist.project_name, shell=False)
